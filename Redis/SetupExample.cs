895901fc4a8726538a78fc524121f05254632796
Rodrigo Pincolini Amaral <rodrigopincoliniamaral@gmail.com>
2023-10-11 05:39:26 -0300

    commit initial

---
+++ Redis/SetupExample.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+
+class SetupExample
+{
+    static void Main()
+    {
+        // Connect to the Redis server
+        var redisServer = ConnectionMultiplexer.Connect("localhost:6379");
+
+        // Get a reference to the Redis database
+        IDatabase db = redisServer.GetDatabase();
+
+        // Set a key-value pair
+        db.StringSet("myKey", "Hello, Redis!");
+
+        // Retrieve a value by key
+        string value = db.StringGet("myKey");
+        Console.WriteLine("Retrieved value: " + value);
+
+        // Delete the key
+        bool keyDeleted = db.KeyDelete("myKey");
+        Console.WriteLine("Key deleted: " + keyDeleted);
+    }
+}
